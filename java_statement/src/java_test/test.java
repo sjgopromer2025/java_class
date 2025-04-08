058ee5439acf6bdf33a5fb3d9b79b41b3aaec33a
유승진 <91680691+sjgopromer2025@users.noreply.github.com>
2025-04-08 14:22:19 +0900

    테스트

---
+++ java_statement/src/java_test/test.java
@@ -0,0 +1,41 @@
+package java_test;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("자바 프로그래밍 시작!");
+		System.out.println("Hello World!");
+
+
+		int number1; // 변수 선언
+		number1 = 90;// 변수 저장
+		int number2 = 90; // 변수 선언과 저장 
+		
+		
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
